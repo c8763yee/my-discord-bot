2f6dbf9bdacc94bda85e8daabd4ea5ea486ccd71
c8763yee <rrrrr4413@gmail.com>
2024-02-27 12:47:24 +0800

    refactor(cogs.pi): formatted by autopep8

--- cogs/pi/tasks.py
+++ cogs/pi/tasks.py
@@ -1,5 +1,5 @@
 import os
-
+import datetime
 from discord.ext import tasks
 
 from cogs import CogsExtension
@@ -10,14 +10,17 @@ from .utils import RaspberryPiUtils
 logger = setup_package_logger(__name__)
 
 
+per_O_clock = datetime.time(minute=0, second=0)
+
+
 class RaspberryPiTasks(CogsExtension):
     def __init__(self, bot):
         super().__init__(bot)
         self.utils = RaspberryPiUtils(bot)
         self.get_temperature.start()
 
-    @tasks.loop(minutes=5)
+    @tasks.loop(time=per_O_clock)
     async def get_temperature(self):
-        channel = self.bot.get_channel(int(os.environ['TEST_CHANNEL_ID']))
+        channel = self.bot.get_channel(int(os.environ["TEST_CHANNEL_ID"]))
         message = await self.utils.get_temperature()
         await channel.send(message)
