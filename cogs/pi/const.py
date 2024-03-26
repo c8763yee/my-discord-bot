7e667a1c909ef6e1db749e51082838aedd53cd6e
c8763yee <rrrrr4413@gmail.com>
2024-03-26 22:34:27 +0800

    fix: permission error for `measure_temp`

--- cogs/pi/const.py
+++ cogs/pi/const.py
@@ -1,3 +1,5 @@
-TEMPERATURE_COMMAND: str = "/usr/bin/vcgencmd measure_temp | awk -F\"=\" '{print $2}' | head -c -3"
+TEMPERATURE_COMMAND: str = (
+    "sudo /usr/bin/vcgencmd measure_temp | awk -F\"=\" '{print $2}' | head -c -3"
+)
 REBOOT_TEMPERATURE: float = 80.0
 WARNING_TEMPERATURE: float = 60.0
